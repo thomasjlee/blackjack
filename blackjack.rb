d77908d9710b0d743a72bb4fe8cb1b6bc7cbe7bd
Thomas Lee <leethomasjae@gmail.com>
2019-04-01 18:25:24 -0500

    this is the gist of a round of blackjack

--- blackjack.rb
+++ blackjack.rb
@@ -87,6 +87,15 @@ class Player
     end
   end
 
+  # TODO: test me
+  def final_hand
+    if bust?
+      possible_hands.min
+    else
+      possible_hands.select { |hand| hand <= 21 }.max
+    end
+  end
+
   private
 
   def hand_without_aces(non_aces)
@@ -103,5 +112,108 @@ class Player
 
   def ace?
     @cards.any? { |card| card.name == :ace }
+  end
+end
+
+class Dealer < Player
+  # TODO: test me
+  def must_stay?
+    if ace?
+      possible_hands.any? { |hand| hand.between?(17, 21) }
+    else
+      hand = possible_hands.first
+      hand >= 17
+    end
+  end
+end
+
+require 'pp'
+
+dealer = Dealer.new
+player = Player.new
+deck = Deck.new
+
+# deal card to player, then dealer (2 cards each)
+2.times do
+  player.deal(deck.draw!)
+  dealer.deal(deck.draw!)
+end
+
+puts "DEALER'S CARDS"
+# dealer's second dealt card is face-down
+pp dealer.cards[0]
+puts '[ x ]'
+
+puts "\nYOUR CARDS"
+pp player.cards
+
+# if player has blackjack, it's the end of the round
+# if dealer also has blackjack
+#   it's a push
+# otherwise
+#   player has won
+
+if player.blackjack?
+  if dealer.blackjack?
+    puts "PUSH!"
+  else
+    puts "YOU WIN!"
+  end
+end
+
+# if player does not have blackjack, we ask: hit or stay?
+if !player.blackjack?
+  hit_or_stay = ''
+  until hit_or_stay == 'stay' || player.bust? || player.twenty_one?
+    puts 'hit or stay?'
+    hit_or_stay = gets.chomp.downcase
+    if hit_or_stay == 'hit'
+      player.deal(deck.draw!)
+
+      # render the hands
+      puts "DEALER'S CARDS"
+      pp dealer.cards[0]
+      puts '[ x ]'
+
+      puts "\nYOUR CARDS"
+      pp player.cards
+    end
+  end
+
+  if player.bust?
+    puts "BUST! YOU LOSE!"
+    return
+  end
+
+  # then it's the dealer's sequence
+  # dealer hits until bust or greater than (or equal to) 17
+
+  until dealer.must_stay? || dealer.bust?
+    puts "... dealer hits ..."
+    dealer.hit(deck.draw!)
+    # now, the dealer's hand is revealed
+    # render the hands
+    puts "DEALER'S CARDS"
+    pp dealer.cards
+
+    puts "\nYOUR CARDS"
+    pp player.cards
+  end
+
+  if dealer.bust?
+    puts "DEALER BUST! YOU WIN!"
+  end
+end
+
+# if neither has busted and the player has not blackjacked
+# (this will be better captured in a loop)
+# then, finally, compare the hands to see who wins
+if !player.blackjack? && !player.bust? && !dealer.bust?
+  if dealer.final_hand > player.final_hand
+    puts "DEALER WINS!"
+  elsif player.final_hand > dealer.final_hand
+    puts "YOU WIN!"
+  else
+    puts "PUSH!"
   end
 end
